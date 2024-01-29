60e30738b57d2ecc00a454e83feb54ca07ddd89d
Leopoldo Garcia Vargas <lk13.dev@gmail.com>
2024-01-29 15:22:06 -0500

    Use chainlit for the chat flow

---
+++ app.py
@@ -0,0 +1,81 @@
+from operator import itemgetter
+import chainlit as cl
+
+from langchain.memory import ConversationBufferMemory
+from langchain_community.chat_models import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda, RunnableConfig
+
+from chainlit.types import ThreadDict
+
+OLLAMA_MODEL = "openhermes"
+SYSTEM_MESSAGE = "You are multilingual expert writer that focuses on helping translate and correct text."
+
+
+def setup_runnable():
+    memory = cl.user_session.get("memory")  # type: ConversationBufferMemory
+    model = ChatOllama(model=OLLAMA_MODEL, streaming=True)
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", SYSTEM_MESSAGE),
+            MessagesPlaceholder(variable_name="history"),
+            ("human", "{question}"),
+        ]
+    )
+
+    runnable = (
+            RunnablePassthrough.assign(
+                history=RunnableLambda(memory.load_memory_variables) | itemgetter("history")
+            )
+            | prompt
+            | model
+            | StrOutputParser()
+    )
+    cl.user_session.set("runnable", runnable)
+
+
+@cl.password_auth_callback
+def auth():
+    return cl.User(identifier="test")
+
+
+@cl.on_chat_start
+async def on_chat_start():
+    cl.user_session.set("memory", ConversationBufferMemory(return_messages=True))
+    setup_runnable()
+
+
+@cl.on_chat_resume
+async def on_chat_resume(thread: ThreadDict):
+    memory = ConversationBufferMemory(return_messages=True)
+    root_messages = [m for m in thread["steps"] if m["parentId"] == None]
+    for message in root_messages:
+        if message["type"] == "USER_MESSAGE":
+            memory.chat_memory.add_user_message(message["output"])
+        else:
+            memory.chat_memory.add_ai_message(message["output"])
+
+    cl.user_session.set("memory", memory)
+
+    setup_runnable()
+
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    memory = cl.user_session.get("memory")  # type: ConversationBufferMemory
+
+    runnable = cl.user_session.get("runnable")  # type: Runnable
+
+    res = cl.Message(content="")
+
+    async for chunk in runnable.astream(
+            {"question": message.content},
+            config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+    ):
+        await res.stream_token(chunk)
+
+    await res.send()
+
+    memory.chat_memory.add_user_message(message.content)
+    memory.chat_memory.add_ai_message(res.content)
